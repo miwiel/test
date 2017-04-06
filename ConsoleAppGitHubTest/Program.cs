3b9be0f66862eb807214e3139b74c899ceff4140
miwiel <miwiel.github@gmail.com>
2017-04-06 20:24:39 +0200

    Second commit.

--- ConsoleAppGitHubTest/Program.cs
+++ ConsoleAppGitHubTest/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppGitHubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello World");
             Console.ReadLine();
         }
     }
